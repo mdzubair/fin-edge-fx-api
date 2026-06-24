f6f82bf1ade96e4f76874daf7b94c42abcd365c0
Mohammad zubair <sumitshrivastwa491@gmail.com>
2026-06-24 16:29:12 +0530

    push again

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,11 +1,9 @@
 import dotenv from "dotenv";
-dotenv.config();
 import dns from "node:dns";
 import mongoose from "mongoose";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
 let isConnected = false;
-
+dotenv.config();
 const connectDB = async (): Promise<void> => {
   try {
     if (isConnected) {
